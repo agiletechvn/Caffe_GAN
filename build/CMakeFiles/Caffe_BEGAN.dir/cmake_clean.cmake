6203c6e0d942b65245bf9af8960e068f3e87977f
Thanh Tu <tu@novalex.vn>
2019-07-05 10:21:18 +0700

    Initial commit

---
+++ build/CMakeFiles/Caffe_BEGAN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caffe_BEGAN.dir/src/NN_Agent.cpp.o"
+  "CMakeFiles/Caffe_BEGAN.dir/src/main.cpp.o"
+  "Caffe_BEGAN.pdb"
+  "Caffe_BEGAN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Caffe_BEGAN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
